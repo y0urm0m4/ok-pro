5fadf854ceb0a277d01bf5ee388e0e1fe7520ded
nasco <kilik2310@gmail.com>
2026-04-29 21:55:27 +0300

    feat: add Yandex Metrika counter 108980267

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope, Cormorant_Garamond } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { SITE_NAME, SITE_DESCRIPTION, SITE_URL } from "@/lib/constants";
 import { Header } from "@/components/layout/Header";
@@ -119,6 +120,16 @@ export default function RootLayout({
         <main>{children}</main>
         <Footer />
         <DiscountPopup />
+        <Script id="metrika" strategy="afterInteractive">{`
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+          ym(108980267,'init',{webvisor:true,clickmap:true,accurateTrackBounce:true,trackLinks:true});
+        `}</Script>
+        <noscript><img src="https://mc.yandex.ru/watch/108980267" style={{position:'absolute',left:'-9999px'}} alt="" /></noscript>
       </body>
     </html>
   );
